c61bf9b4d9509cb7c441d1a7e2cca85db901d054
Aline F. Pinotti <164499124+Aline-Pinotti@users.noreply.github.com>
2024-05-07 11:43:52 -0300

    Update insercao_de_dados_e_queries_sql.sql
    Alterada a ordem de inclusão em employee, devido à foreing key do supervisor/gerente

--- Módulo 3/Desafio de Projeto/insercao_de_dados_e_queries_sql.sql
+++ Módulo 3/Desafio de Projeto/insercao_de_dados_e_queries_sql.sql
@@ -1,13 +1,24 @@
 use company_constraints;
 
-insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
-							('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+/*insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
+			    ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
                             ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
                             ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
                             ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
                             ('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631-Rice-Houston-TX', 'F', 25000, 333445555, 5),
                             ('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980-Dallas-Houston-TX', 'M', 25000, 987654321, 4),
-                            ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1);
+                            ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1);*/
+
+--substituído para evitar erro de constraint (informar chave estrangeira de registro que nem foi criado ainda), alterada a ordem
+insert into employee values ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1),
+                            ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+                            ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
+                            ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
+                            ('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980-Dallas-Houston-TX', 'M', 25000, 987654321, 4),							
+                            ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
+                            ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
+                            ('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631-Rice-Houston-TX', 'F', 25000, 333445555, 5);
+
 
 insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter'),
 							 (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
@@ -28,8 +39,8 @@ insert into dept_locations values (1, 'Houston'),
                                  (5, 'Houston');
 
 insert into project values ('ProductX', 1, 'Bellaire', 5),
-						   ('ProductY', 2, 'Sugarland', 5),
-						   ('ProductZ', 3, 'Houston', 5),
+			   ('ProductY', 2, 'Sugarland', 5),
+			   ('ProductZ', 3, 'Houston', 5),
                            ('Computerization', 10, 'Stafford', 4),
                            ('Reorganization', 20, 'Houston', 1),
                            ('Newbenefits', 30, 'Stafford', 4)
